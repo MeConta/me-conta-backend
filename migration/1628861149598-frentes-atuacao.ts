e23db4d541f2de57a040321e5c27f8528b3ba5de
Diego Lopes <diego.lopes@thoughtworks.com>
2021-08-13 10:33:05 -0300

    [#8] Frentes de atuação - migração closes #8
    Criação da migração para preenchimento das frentes de atuação

---
+++ migration/1628861149598-frentes-atuacao.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class frentesAtuacao1628861149598 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`INSERT INTO public."frente-atuacao" (id, "dataCriacao", "dataAlteracao", "dataExclusao", nome, descricao)
+        VALUES (DEFAULT, DEFAULT, DEFAULT, null, 'Sessões de acolhimento dos estudantes',
+            'Sessões de acolhimento dos estudantes')`);
+
+    await queryRunner.query(`INSERT INTO public."frente-atuacao" (id, "dataCriacao", "dataAlteracao", "dataExclusao", nome, descricao)
+        VALUES (DEFAULT, DEFAULT, DEFAULT, null, 'Coaching de rotina de estudos', 'Coaching de rotina de estudos')`);
+
+    await queryRunner.query(`INSERT INTO public."frente-atuacao" (id, "dataCriacao", "dataAlteracao", "dataExclusao", nome, descricao)
+        VALUES (DEFAULT, DEFAULT, DEFAULT, null, 'Orientação vocacional', 'Orientação vocacional')`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DELETE FROM public."frente-atuacao" WHERE nome = 'Sessões de acolhimento dos estudantes'`,
+    );
+
+    await queryRunner.query(
+      `DELETE FROM public."frente-atuacao" WHERE nome = 'Coaching de rotina de estudos'`,
+    );
+
+    await queryRunner.query(
+      `DELETE FROM public."frente-atuacao" WHERE nome = 'Orientação vocacional'`,
+    );
+  }
+}
