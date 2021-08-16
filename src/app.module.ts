a0fa9bdfeec6ecde1b649f2721b00f3258f6d027
Diego Lopes <diego.lopes@thoughtworks.com>
2021-08-16 15:53:53 -0300

    remoção de arquivos iniciais desnecessários

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsuarioModule } from './usuario/usuario.module';
