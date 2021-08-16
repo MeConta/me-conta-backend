b71f90f726f721ca7fb423622960d6516d8eddf7
Diego Lopes <diego.lopes@thoughtworks.com>
2021-08-16 18:22:37 -0300

    [#11] criação do controller de aluno

--- src/aluno/aluno.controller.ts
+++ src/aluno/aluno.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { AlunoService } from './aluno.service';
 import { CreateAlunoDto } from './dto/create-aluno.dto';
+import { UpdateAlunoDto } from './dto/update-aluno.dto';
 
 @Controller('aluno')
 export class AlunoController {
@@ -9,5 +20,26 @@ export class AlunoController {
   @Post()
   async create(@Body() dto: CreateAlunoDto) {
     return this.alunoService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.alunoService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.alunoService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateAlunoDto) {
+    return this.alunoService.update(+id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    return this.alunoService.remove(+id);
   }
 }
