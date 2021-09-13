e82fa58c1ca400a1c6200e78fb9008403987057d
Diego Lopes <diego.lopes@thoughtworks.com>
2021-09-13 12:41:01 -0300

    fix(#23): ajustes de swagger para o atendente - closes #23

--- src/voluntario/dto/create-voluntario.dto.ts
+++ src/voluntario/dto/create-voluntario.dto.ts
@@ -2,8 +2,7 @@ import { CreateUsuarioDto } from '../../usuario/dto/create-usuario.dto';
 import { ArrayMinSize, IsArray, IsNotEmpty } from 'class-validator';
 import { FrenteAtuacao } from '../../frente-atuacao/entities/frente-atuacao.entity';
 import { Transform, Type } from 'class-transformer';
-import { OmitType } from '@nestjs/mapped-types';
-import { ApiProperty } from '@nestjs/swagger';
+import { OmitType, ApiProperty } from '@nestjs/swagger';
 
 export class CreateVoluntarioDto extends OmitType(CreateUsuarioDto, [
   'tipoUsuario',
