35c1b6c1d9532233b2db2d249961fd334b50039a
Diego Lopes <diego.lopes@thoughtworks.com>
2021-09-30 16:13:23 -0300

    refactor(#27): criação de testes e2e

--- test/criar-conta.e2e-spec.ts
+++ test/criar-conta.e2e-spec.ts
@@ -1,37 +1,69 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { UsuarioStub } from '../src/testing/usuario.stub';
-import { DbE2eModule } from './db.e2e.module';
 import { CadastroInicialModule } from '../src/cadastro-inicial/cadastro-inicial.module';
 import { CreateUsuarioDto } from '../src/_adapters/usuarios/dto/create-usuario.dto';
 import { TipoUsuario } from '../src/_business/usuarios/casos-de-uso/cadastrar-novo-usuario.feat';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioDbEntity } from '../src/_adapters/usuarios/entidades/usuario.db.entity';
+import { setupApp } from '../src/config/app.config';
 
 describe('Criar Conta (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [DbE2eModule, CadastroInicialModule],
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'better-sqlite3',
+          database: ':memory:',
+          dropSchema: true,
+          logging: false,
+          autoLoadEntities: true,
+          entities: [UsuarioDbEntity],
+          synchronize: true,
+        }),
+        CadastroInicialModule,
+      ],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    console.log(app);
+    app = await moduleFixture.createNestApplication();
+    setupApp(app);
     await app.init();
   });
 
-  it('/usuario (POST)', async () => {
-    const response = await request(app.getHttpServer())
-      .post('/cadastro-inicial')
-      .send({
-        nome: 'Teste',
-        email: 'teste@teste.com',
-        senha: 'SenH4F@rt33',
-        tipo: TipoUsuario.ATENDENTE,
-      } as CreateUsuarioDto)
-      .expect(201);
+  describe('/cadastro-inicial (POST)', () => {
+    const req = {
+      nome: 'Teste',
+      email: 'teste@teste.com',
+      senha: 'SenH4F@rt33',
+      tipo: TipoUsuario.ATENDENTE,
+    } as CreateUsuarioDto;
 
-    expect(response.body.nome).toBe(UsuarioStub.getEntity().nome);
+    it('Deve Cadastrar um usuário', async () => {
+      await request(app.getHttpServer())
+        .post('/cadastro-inicial')
+        .send(req)
+        .expect(201);
+    });
+
+    it('Deve dar erro 400', async () => {
+      await request(app.getHttpServer())
+        .post('/cadastro-inicial')
+        .send({ ...req, nome: null } as CreateUsuarioDto)
+        .expect(400);
+    });
+    it('Deve dar erro 409', async () => {
+      await request(app.getHttpServer())
+        .post('/cadastro-inicial')
+        .send(req)
+        .expect(201);
+
+      await request(app.getHttpServer())
+        .post('/cadastro-inicial')
+        .send(req)
+        .expect(409);
+    });
   });
 
   afterEach(async () => {
