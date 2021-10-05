4f23bde0c4a8b17339221298aa98ec92f87b0ebb
Diego Lopes <diego.lopes@thoughtworks.com>
2021-10-05 11:16:46 -0300

    feat: inclusão de tamanho máximo do nome

--- src/_adapters/usuarios/dto/create-usuario.dto.ts
+++ src/_adapters/usuarios/dto/create-usuario.dto.ts
@@ -1,11 +1,17 @@
-import { IsEmail, IsEnum, IsNotEmpty, MinLength } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-import { Erros } from '../../../config/constants';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import {
   NovoUsuario,
   TipoUsuario,
 } from '../../../_business/usuarios/casos-de-uso/cadastrar-novo-usuario.feat';
 import { IsPasswordStrong } from '../../../decorators';
+import { ApiProperty } from '@nestjs/swagger';
+import { Erros } from '../../../config/constants';
 
 export class CreateUsuarioDto implements NovoUsuario {
   /***
@@ -14,6 +20,9 @@ export class CreateUsuarioDto implements NovoUsuario {
    */
   @IsNotEmpty()
   @MinLength(2)
+  @MaxLength(100, {
+    message: '$property deve ter menos de 300 caracteres',
+  })
   nome: string;
 
   /***
