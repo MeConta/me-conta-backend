e6910374fbebb2e73db8dd78b878b2a9417e0501
Diego Lopes <diego.lopes@thoughtworks.com>
2021-10-21 15:39:59 -0300

    fix: ajuste de decorator de password

--- src/decorators/password.decorator.ts
+++ src/decorators/password.decorator.ts
@@ -1,4 +1,4 @@
-import { registerDecorator, ValidationOptions } from 'class-validator';
+import { isEmpty, registerDecorator, ValidationOptions } from 'class-validator';
 import * as zxcvbn from 'zxcvbn';
 
 export function IsPasswordStrong(
@@ -12,7 +12,10 @@ export function IsPasswordStrong(
       constraints: [],
       options: validationOptions,
       validator: {
-        validate(value: any) {
+        validate(value: string) {
+          if (isEmpty(value)) {
+            return false;
+          }
           return (
             zxcvbn(value).score > (minScore || +process.env.PASSWORD_STRENGTH)
           );
