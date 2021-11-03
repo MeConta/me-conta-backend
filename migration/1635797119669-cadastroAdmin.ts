b3357b7fd0f0d9d896aa14b4c6ead1b8e5225769
Diego Lopes <diego.lopes@thoughtworks.com>
2021-11-03 18:54:05 -0300

    fix: deixar o cadastro de admin inicial opcional a depender do ambiente

--- migration/1635797119669-cadastroAdmin.ts
+++ migration/1635797119669-cadastroAdmin.ts
@@ -1,17 +1,23 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 export class cadastroAdmin1635797119669 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
+    if (
+      process.env.ADMIN_EMAIL &&
+      process.env.ADMIN_PASSWORD_HASHED &&
+      process.env.ADMIN_PASSWORD_SALT
+    ) {
+      await queryRunner.query(`
       INSERT INTO public.usuario ("dataCriacao", "dataAlteracao", "dataExclusao", id, nome, email, senha, salt, tipo,
                                   "dataTermos")
       VALUES (DEFAULT, DEFAULT, null, DEFAULT, 'admin', '${
         process.env.ADMIN_EMAIL
       }',
               '${process.env.ADMIN_PASSWORD_HASHED}', '${
-      process.env.ADMIN_PASSWORD_SALT
-    }',
+        process.env.ADMIN_PASSWORD_SALT
+      }',
               '3'::usuario_tipo_enum, '${new Date().toISOString()}');
     `);
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
