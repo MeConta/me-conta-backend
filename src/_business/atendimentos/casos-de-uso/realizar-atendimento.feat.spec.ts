94aab88ba4f318e96e96eb8e933eac6e2e5e7416
Alexandre Araujo <alexandreh.araujo@gmail.com>
2021-12-22 08:37:15 -0300

    feat(#54): testa voluntario não encontrado ao realizar atendimento

--- src/_business/atendimentos/casos-de-uso/realizar-atendimento.feat.spec.ts
+++ src/_business/atendimentos/casos-de-uso/realizar-atendimento.feat.spec.ts
@@ -2,6 +2,9 @@ import { createMock } from '@golevelup/ts-jest';
 import { Atendimento, NovoAtendimento } from '../entidades/atendimentos.entity';
 import { IRealizarNovoAtendimentoService } from '../../../_adapters/atendimentos/services/atendimentos.service';
 import { RealizarAtendimento } from './realizar-atendimento.feat';
+import { VoluntarioNaoEncontradoError } from '../../admin/casos-de-uso/autorizar-voluntario.feat';
+import { IBuscarUsuarioViaId } from '../../usuarios/casos-de-uso/buscar-usuario.id.feat';
+import { IBuscarVoluntarioViaId } from '../../voluntarios/services/voluntario.service';
 
 class InMemoryAtendimentoService implements IRealizarNovoAtendimentoService {
   public atendimentos: Atendimento[] = [];
@@ -16,11 +19,13 @@ class InMemoryAtendimentoService implements IRealizarNovoAtendimentoService {
 describe('Realizar atendimento', () => {
   let sut: RealizarAtendimento;
   let atendimentoService: InMemoryAtendimentoService;
+  let voluntarioService: IBuscarVoluntarioViaId;
   const request = createMock<NovoAtendimento>();
 
   beforeEach(async () => {
     atendimentoService = new InMemoryAtendimentoService();
-    sut = new RealizarAtendimento(atendimentoService);
+    voluntarioService = createMock<IBuscarVoluntarioViaId>();
+    sut = new RealizarAtendimento(atendimentoService, voluntarioService);
   });
   it('Deve ser Definido', () => {
     expect(sut).toBeDefined();
@@ -29,5 +34,12 @@ describe('Realizar atendimento', () => {
   it('Deve realizar um atendimento', async () => {
     await sut.execute(request);
     expect(atendimentoService.atendimentos[0]).toBeDefined();
+  });
+
+  it('Não deve realizar o atendimento se o  voluntário não existir', async () => {
+    jest.spyOn(voluntarioService, 'findById').mockResolvedValue(null);
+    await expect(() => sut.execute(request)).rejects.toThrow(
+      VoluntarioNaoEncontradoError,
+    );
   });
 });
--- src/_business/atendimentos/casos-de-uso/realizar-atendimento.feat.ts
+++ src/_business/atendimentos/casos-de-uso/realizar-atendimento.feat.ts
@@ -1,11 +1,23 @@
 import { NovoAtendimento } from '../entidades/atendimentos.entity';
 import { IRealizarNovoAtendimentoService } from '../../../_adapters/atendimentos/services/atendimentos.service';
+import { IBuscarVoluntarioViaId } from '../../voluntarios/services/voluntario.service';
+import { VoluntarioNaoEncontradoError } from '../../admin/casos-de-uso/autorizar-voluntario.feat';
 
 export class RealizarAtendimento {
   constructor(
     private readonly atendimentoService: IRealizarNovoAtendimentoService,
+    private readonly voluntarioService: IBuscarVoluntarioViaId,
   ) {}
-  async execute(input: NovoAtendimento): Promise<void> {
-    await this.atendimentoService.realizar(input);
+  async execute(novoAtendimento: NovoAtendimento): Promise<void> {
+    const voluntario = await this.voluntarioService.findById(
+      (
+        await novoAtendimento.voluntario
+      ).id,
+    );
+
+    if (!voluntario) {
+      throw new VoluntarioNaoEncontradoError();
+    }
+    await this.atendimentoService.realizar(novoAtendimento);
   }
 }
