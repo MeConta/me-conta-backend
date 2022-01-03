534baa77265876991482a4b51c7ad1b199f6f339
Alexandre Araujo <alexandreh.araujo@gmail.com>
2022-01-03 14:19:26 -0300

    feat(#57): teste de aluno não encontrado

--- src/_business/atendimentos/casos-de-uso/historico-atendimento.feat.spec.ts
+++ src/_business/atendimentos/casos-de-uso/historico-atendimento.feat.spec.ts
@@ -1,7 +1,10 @@
 import { createMock } from '@golevelup/ts-jest';
 import { Atendimento } from '../entidades/atendimentos.entity';
 import { HistoricoAtendimento } from './historico-atendimento.feat';
-import { IBuscarAlunoViaId } from '../../alunos/casos-de-uso/atualizar-aluno.feat';
+import {
+  AlunoNaoEncontradoError,
+  IBuscarAlunoViaId,
+} from '../../alunos/casos-de-uso/atualizar-aluno.feat';
 import { IHistoricoAtendimentoService } from '../../../_adapters/atendimentos/services/atendimentos.service';
 
 class InMemoryAtendimentoService implements IHistoricoAtendimentoService {
@@ -21,7 +24,13 @@ describe('Historico atendimentos', () => {
     alunoService = createMock<IBuscarAlunoViaId>();
     sut = new HistoricoAtendimento(alunoService, atendimentoService);
   });
+
   it('Deve ser Definido', () => {
     expect(sut).toBeDefined();
+  });
+
+  it('Não deve buscar o histórico se o aluno não existir', async () => {
+    jest.spyOn(alunoService, 'findById').mockResolvedValue(null);
+    await expect(() => sut.execute(1)).rejects.toThrow(AlunoNaoEncontradoError);
   });
 });
