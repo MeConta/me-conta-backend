6e141755cc46dd5a71bb2d1df6c6c70b07360b7d
Mateus Brito <mateusbrito4@gmail.com>
2022-01-06 18:13:27 -0300

    feat(#58): continua implementação do business de atualizar status do atendimento

--- src/_business/atendimentos/casos-de-uso/atualizar-atendimento.feat.ts
+++ src/_business/atendimentos/casos-de-uso/atualizar-atendimento.feat.ts
@@ -1,21 +1,29 @@
-import { IBuscarAtendimentoViaIdService } from '../../../_adapters/atendimentos/services/atendimentos.service';
+import {
+  IAtualizarStatusAtendimentoService,
+  IBuscarAtendimentoViaIdService
+} from '../../../_adapters/atendimentos/services/atendimentos.service';
 
-import { StatusAtendimento } from '../entidades/atendimentos.entity';
+import {StatusAtendimento} from '../entidades/atendimentos.entity';
 
 export class AtendimentoNaoEncontradoError extends Error {
-  public code = 404;
-  public message = 'Atendimento não encontrado';
+    public code = 404;
+    public message = 'Atendimento não encontrado';
 }
 
 export class AtualizarAtendimento {
-  constructor(
-    private readonly atendimentoService: IBuscarAtendimentoViaIdService,
-  ) {}
-  async execute(id: number, status: StatusAtendimento): Promise<void> {
-    const atendimento = await this.atendimentoService.buscar(id);
-
-    if (!atendimento) {
-      throw new AtendimentoNaoEncontradoError();
+    constructor(
+        private readonly atendimentoService: IBuscarAtendimentoViaIdService &
+            IAtualizarStatusAtendimentoService,
+    ) {
     }
-  }
+
+    async execute(id: number, status: StatusAtendimento): Promise<void> {
+        const atendimento = await this.atendimentoService.buscar(id);
+
+        if (!atendimento) {
+            throw new AtendimentoNaoEncontradoError();
+        }
+
+        await this.atendimentoService.atualizarStatus(id, status);
+    }
 }
