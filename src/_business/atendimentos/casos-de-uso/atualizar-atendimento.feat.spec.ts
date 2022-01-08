40bc9f0c545473ffb7e02b493b3a0d7a6b5cf00f
Alexandre Araujo <alexandreh.araujo@gmail.com>
2022-01-08 09:34:46 -0300

    style: corrige errors de lint com Prettier

--- src/_business/atendimentos/casos-de-uso/atualizar-atendimento.feat.spec.ts
+++ src/_business/atendimentos/casos-de-uso/atualizar-atendimento.feat.spec.ts
@@ -2,60 +2,70 @@ import {
   IAtualizarStatusAtendimentoService,
   IBuscarAtendimentoViaIdService,
 } from '../../../_adapters/atendimentos/services/atendimentos.service';
-import {Atendimento, StatusAtendimento,} from '../entidades/atendimentos.entity';
-import {AtendimentoNaoEncontradoError, AtualizarAtendimento,} from './atualizar-atendimento.feat';
-import {createMock} from '@golevelup/ts-jest';
+import {
+  Atendimento,
+  StatusAtendimento,
+} from '../entidades/atendimentos.entity';
+import {
+  AtendimentoNaoEncontradoError,
+  AtualizarAtendimento,
+} from './atualizar-atendimento.feat';
+import { createMock } from '@golevelup/ts-jest';
 
 class InMemoryAtendimentoService
-    implements IBuscarAtendimentoViaIdService, IAtualizarStatusAtendimentoService {
-    public atendimento: Atendimento = {
-        ...createMock<Atendimento>(),
-        status: StatusAtendimento.ABERTO
-    };
+  implements IBuscarAtendimentoViaIdService, IAtualizarStatusAtendimentoService
+{
+  public atendimento: Atendimento = {
+    ...createMock<Atendimento>(),
+    status: StatusAtendimento.ABERTO,
+  };
 
-    buscar(id: number): Promise<Atendimento> {
-        if (id == 0) {
-            return null;
-        }
-        return Promise.resolve(this.atendimento);
+  buscar(id: number): Promise<Atendimento> {
+    if (id == 0) {
+      return null;
     }
+    return Promise.resolve(this.atendimento);
+  }
 
-    async atualizarStatus(id: number, status: StatusAtendimento): Promise<Atendimento> {
-        this.atendimento = {
-            ...this.atendimento,
-            status: status,
-        } as Atendimento;
-        return this.atendimento;
-    }
+  async atualizarStatus(
+    id: number,
+    status: StatusAtendimento,
+  ): Promise<Atendimento> {
+    this.atendimento = {
+      ...this.atendimento,
+      status: status,
+    } as Atendimento;
+    return this.atendimento;
+  }
 }
 
 describe('Atualizar atendimento', () => {
-    let atendimentoService: InMemoryAtendimentoService;
-    let sut: AtualizarAtendimento;
+  let atendimentoService: InMemoryAtendimentoService;
+  let sut: AtualizarAtendimento;
 
-    beforeEach(async () => {
-        atendimentoService = new InMemoryAtendimentoService();
-        sut = new AtualizarAtendimento(atendimentoService);
-    });
+  beforeEach(async () => {
+    atendimentoService = new InMemoryAtendimentoService();
+    sut = new AtualizarAtendimento(atendimentoService);
+  });
 
-    it('Deve ser definido', () => {
-        expect(sut).toBeDefined();
-    });
+  it('Deve ser definido', () => {
+    expect(sut).toBeDefined();
+  });
 
-    it('Deve dar erro de atendimento não encontrado', async () => {
-        jest.spyOn(atendimentoService, 'buscar').mockResolvedValue(null);
-        await expect(() =>
-            sut.execute(0, expect.any(AtualizarAtendimento)),
-        ).rejects.toThrow(AtendimentoNaoEncontradoError);
-    });
+  it('Deve dar erro de atendimento não encontrado', async () => {
+    jest.spyOn(atendimentoService, 'buscar').mockResolvedValue(null);
+    await expect(() =>
+      sut.execute(0, expect.any(AtualizarAtendimento)),
+    ).rejects.toThrow(AtendimentoNaoEncontradoError);
+  });
 
-    it('Deve atualizar um atendimento', async () => {
-        const status = StatusAtendimento.REALIZADO;
-        await sut.execute(1, status);
-        expect(atendimentoService.atendimento).toEqual(
-            expect.objectContaining({
-                status,
-            } as Atendimento),
-        );
-    });
+  it('Deve atualizar um atendimento', async () => {
+    const status = StatusAtendimento.REALIZADO;
+    await sut.execute(1, status);
+    expect(atendimentoService.atendimento).toEqual(
+      expect.objectContaining({
+        status,
+      } as Atendimento),
+    );
+  });
 });
--- src/_business/atendimentos/casos-de-uso/atualizar-atendimento.feat.ts
+++ src/_business/atendimentos/casos-de-uso/atualizar-atendimento.feat.ts
@@ -1,29 +1,28 @@
 import {
   IAtualizarStatusAtendimentoService,
-  IBuscarAtendimentoViaIdService
+  IBuscarAtendimentoViaIdService,
 } from '../../../_adapters/atendimentos/services/atendimentos.service';
 
-import {StatusAtendimento} from '../entidades/atendimentos.entity';
+import { StatusAtendimento } from '../entidades/atendimentos.entity';
 
 export class AtendimentoNaoEncontradoError extends Error {
-    public code = 404;
-    public message = 'Atendimento não encontrado';
+  public code = 404;
+  public message = 'Atendimento não encontrado';
 }
 
 export class AtualizarAtendimento {
-    constructor(
-        private readonly atendimentoService: IBuscarAtendimentoViaIdService &
-            IAtualizarStatusAtendimentoService,
-    ) {
+  constructor(
+    private readonly atendimentoService: IBuscarAtendimentoViaIdService &
+      IAtualizarStatusAtendimentoService,
+  ) {}
+
+  async execute(id: number, status: StatusAtendimento): Promise<void> {
+    const atendimento = await this.atendimentoService.buscar(id);
+
+    if (!atendimento) {
+      throw new AtendimentoNaoEncontradoError();
     }
 
-    async execute(id: number, status: StatusAtendimento): Promise<void> {
-        const atendimento = await this.atendimentoService.buscar(id);
-
-        if (!atendimento) {
-            throw new AtendimentoNaoEncontradoError();
-        }
-
-        await this.atendimentoService.atualizarStatus(id, status);
-    }
+    await this.atendimentoService.atualizarStatus(id, status);
+  }
 }
