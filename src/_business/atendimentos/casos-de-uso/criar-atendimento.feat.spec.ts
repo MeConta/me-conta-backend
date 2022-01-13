36a06d4ecb59d935b986d2089355f7ebc02fb80f
Alexandre Araujo <alexandreh.araujo@gmail.com>
2022-01-13 08:16:14 -0300

    feat(#54): testa a definição do status padrão (aberto)

--- src/_business/atendimentos/casos-de-uso/criar-atendimento.feat.spec.ts
+++ src/_business/atendimentos/casos-de-uso/criar-atendimento.feat.spec.ts
@@ -1,5 +1,8 @@
 import { createMock } from '@golevelup/ts-jest';
-import { NovoAtendimento } from '../entidades/atendimentos.entity';
+import {
+  NovoAtendimento,
+  StatusAtendimento,
+} from '../entidades/atendimentos.entity';
 import { INovoAtendimentoService } from '../../../_adapters/atendimentos/services/atendimentos.service';
 import {
   AtendimentoNaoAconteceuError,
@@ -29,7 +32,7 @@ describe('Criar atendimento', () => {
   let agendaService: IBuscarSlotAgendaByIdService;
   let alunoService: IBuscarAlunoViaId;
   let dateService: IDateGreaterThan;
-  const request = createMock<NovoAtendimento>();
+  const novoAtendimento = createMock<NovoAtendimento>();
 
   beforeEach(async () => {
     atendimentoService = new InMemoryAtendimentoService();
@@ -49,27 +52,36 @@ describe('Criar atendimento', () => {
 
   it('Deve criar um atendimento', async () => {
     jest.spyOn(dateService, 'greaterThan').mockReturnValue(false);
-    await sut.execute(request);
+    await sut.execute(novoAtendimento);
     expect(atendimentoService.atendimentos[0]).toBeDefined();
+  });
+
+  it('Deve adicionar status EM ABERTO ao atendimento', async () => {
+    jest.spyOn(dateService, 'greaterThan').mockReturnValue(false);
+    await sut.execute(novoAtendimento);
+    expect(atendimentoService.atendimentos[0]).toHaveProperty(
+      'status',
+      StatusAtendimento.ABERTO,
+    );
   });
 
   it('Não deve criar o atendimento se a agenda não existir', async () => {
     jest.spyOn(agendaService, 'findById').mockResolvedValue(null);
-    await expect(() => sut.execute(request)).rejects.toThrow(
+    await expect(() => sut.execute(novoAtendimento)).rejects.toThrow(
       SlotAgendaNaoEncontradoError,
     );
   });
 
   it('Não deve criar o atendimento se o aluno não existir', async () => {
     jest.spyOn(alunoService, 'findById').mockResolvedValue(null);
-    await expect(() => sut.execute(request)).rejects.toThrow(
+    await expect(() => sut.execute(novoAtendimento)).rejects.toThrow(
       AlunoNaoEncontradoError,
     );
   });
 
   it('Não deve criar o atendimento se a consulta for no futuro', async () => {
     jest.spyOn(dateService, 'greaterThan').mockReturnValue(true);
-    await expect(() => sut.execute(request)).rejects.toThrow(
+    await expect(() => sut.execute(novoAtendimento)).rejects.toThrow(
       AtendimentoNaoAconteceuError,
     );
   });
